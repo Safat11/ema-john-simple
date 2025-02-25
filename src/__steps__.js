d2870cd3eeb759eaf638977c2c4aaa5234cbdfbf
Safat11 <safat3622@gmail.com>
2025-02-25 23:35:17 +0600

    Host Your React App To Firebase And Show Password

--- src/__steps__.js
+++ src/__steps__.js
@@ -14,3 +14,25 @@
  * 4. Set context value
  * 5. Set the provider
 */
+
+/**
+ * HOSTING
+ * -------------------
+ * One time per PC
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 
+ * For each project one time
+ * 1. firebase init
+ * 2. proceed
+ * 3. Hosting: firebase (up and down arrow) use space bar to select
+ * 4. existing project
+ * 5. select project carefully
+ * 6. which project as public directory: dist
+ * 7. single page application: yes
+ * 8. continuous deployment: no
+ * 
+ * For every time deploy
+ * 1. npm run build
+ * firebase deploy
+ */
